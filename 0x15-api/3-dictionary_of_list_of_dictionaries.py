5c4a4614fe0be317c0a7f22db9501c4a478a7467
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-24 23:51:06 +0100

    convert all employees data to todolsit file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Use the Rest api to get employee todo list
+    and export all completed todo to csv file
+"""
+import csv
+import json
+import os
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + "users").json()
+    todos = requests.get(url + "todos").json()
+    file = "todo_all_employees.json"
+    with open(file, 'w') as file:
+        for user in users:
+            todos = requests.get(
+                    url + "todos", params={"userId": user.get('id')}).json()
+            json.dump({user.get('id'): [{"username": user.get('name'),
+                                        "task": todo.get('title'),
+                                         "completed": todo.get("completed")}
+                                        for todo in todos]}, file)
