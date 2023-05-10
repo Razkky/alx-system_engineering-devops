d4f74c09b1291192eaacb23dafa11fe352a7640e
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-10 12:48:58 +0100

    fetch top then post using reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module uses the reddit api to fetch data"""
+
+import requests
+
+
+def top_ten(subreddit):
+    posts = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
+    if posts.status_code >= 300:
+        print("None")
+    children = posts.json().get('data').get('children')
+    [print(child.get('data').get('title'))
+        for child in children]
