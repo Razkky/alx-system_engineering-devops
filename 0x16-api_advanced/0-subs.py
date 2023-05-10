76285416cd863aabb5b32a76239dcd851cf894e8
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-10 11:40:54 +0100

    check for valid subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,8 @@ def number_of_subscribers(subreddit):
                  "https://www.reddit.com/r/{}/about.json".format(subreddit),
                  headers={"User-Agent": "My-User-Agent"},
                  allow_redirects=False)
+        if abouts.status_code >= 300:
+            return 0
         abouts = abouts.json()
         subscriber = abouts['data']['subscribers']
         return subscriber
