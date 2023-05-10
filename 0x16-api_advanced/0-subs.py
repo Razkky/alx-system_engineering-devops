b80932fe273e1727365599c8d678c422df4ebd08
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-10 11:34:09 +0100

    get number of subscribers using reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""This module uses the redit api to get th number of subscribers
+    for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """This function uses the request module to access the redit api"""
+    if subreddit:
+        abouts = requests.get(
+                 "https://www.reddit.com/r/{}/about.json".format(subreddit),
+                 headers={"User-Agent": "My-User-Agent"},
+                 allow_redirects=False)
+        abouts = abouts.json()
+        subscriber = abouts['data']['subscribers']
+        return subscriber
