0ea4632dad4fd5daa211db9924aa43ac1e775647
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-10 13:43:06 +0100

    use recursion to fetch reddit api

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module uses recursion to quarry reddit api"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="None"):
+    """Get all the title of a subreddit"""
+
+    posts = requests.get(
+        "https://www.reddit.com/r/{}/hot.json"
+        .format(subreddit),
+        params={"after": after},
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+         )
+    if posts.status_code >= 300:
+        return None
+    children = posts.json().get('data').get('children')
+    hot_list.append([child.get('data').get('title')
+                    for child in children])
+    after = posts.json().get('data').get('after')
+    if not after:
+        return hot_list
+    recurse(subreddit, hot_list, after)
+    return hot_list
