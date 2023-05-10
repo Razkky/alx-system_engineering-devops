961e5dcf0cb7a8ce73e490775e8f1ec5401fde3c
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-10 12:50:46 +0100

    ducoment function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ import requests
 
 
 def top_ten(subreddit):
+    """Fetch top 10 posts using reddit api"""
     posts = requests.get(
         "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
         headers={"User-Agent": "My-User-Agent"},
